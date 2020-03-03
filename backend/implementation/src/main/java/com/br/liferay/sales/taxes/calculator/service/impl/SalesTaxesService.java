d1c3ceeb117abe6c909b87594dc5283a9ea5c9a0
Rafael Batista Duarte <rafaelbatistaduarte@gmail.com>
2020-03-02 22:48:30 -0300

    Validations

--- backend/implementation/src/main/java/com/br/liferay/sales/taxes/calculator/service/impl/SalesTaxesService.java
+++ backend/implementation/src/main/java/com/br/liferay/sales/taxes/calculator/service/impl/SalesTaxesService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.br.liferay.sales.taxes.calculator.exception.CustomException;
 import com.br.liferay.sales.taxes.calculator.model.Product;
 import com.br.liferay.sales.taxes.calculator.model.SalesTaxesProductDefinition;
 import com.br.liferay.sales.taxes.calculator.model.SalesTaxesProductsDefinition;
@@ -13,83 +14,90 @@ import com.br.liferay.sales.taxes.calculator.utils.Constants;
 import com.br.liferay.sales.taxes.calculator.utils.Utils;
 
 @Service
-public class SalesTaxesService{
-	
+public class SalesTaxesService {
+
 	private IProductRepository iProductRepository;
-	
-	public SalesTaxesService (IProductRepository iProductRepository) {
+
+	public SalesTaxesService(IProductRepository iProductRepository) {
 		this.iProductRepository = iProductRepository;
 	}
 
-	public SalesTaxesProductsDefinition calculateSalesTaxes(List<String> productsDescriptions)
-	{
-		SalesTaxesProductsDefinition salesTaxesProductsDefinition = new SalesTaxesProductsDefinition();
-		for (String productDescription : productsDescriptions) {
-			SalesTaxesProductDefinition salesTaxesProductDefinition = productDescriptionParser(productDescription);
-			calculateSalesTaxes(salesTaxesProductDefinition);
-			salesTaxesProductsDefinition.getSalesTaxesProductsDefinition().add(salesTaxesProductDefinition);
-		}
-		return salesTaxesProductsDefinition;
-	}
-	
-	private void calculateSalesTaxes(SalesTaxesProductDefinition salesTaxesProductDefinition) {
-		Optional<Product> product = iProductRepository.findByNameIgnoreCase(salesTaxesProductDefinition.getProductName());
-		if(!product.isPresent())
-		{
-			//TODO: throw a custom exception
-		}else
-		{
-			double tax = salesTaxesProductDefinition.getPrice()*(product.get().getProductType().getTax()/100);
-			
-			if(salesTaxesProductDefinition.isImported())
-			{
-				tax += salesTaxesProductDefinition.getPrice()*(Constants.IMPORTED_PRODUCT_TAX/100);
+	public SalesTaxesProductsDefinition calculateSalesTaxes(List<String> productsDescriptions) {
+		int line = 1;
+		try {
+			SalesTaxesProductsDefinition salesTaxesProductsDefinition = new SalesTaxesProductsDefinition();
+			for (String productDescription : productsDescriptions) {
+				SalesTaxesProductDefinition salesTaxesProductDefinition = productDescriptionParser(productDescription);
+				calculateSalesTaxes(salesTaxesProductDefinition);
+				salesTaxesProductsDefinition.getSalesTaxesProductsDefinition().add(salesTaxesProductDefinition);
+				line++;
 			}
-			
-			tax = Utils.ceil(tax);
-			salesTaxesProductDefinition.setPriceWithTax(Utils.round(salesTaxesProductDefinition.getPrice()+tax));
+
+			return salesTaxesProductsDefinition;
+		} catch (CustomException e) {
+			// TODO: Change this to a error msg propertie
+			throw new CustomException("Line: "+line+", error message: "+e.getMessage());
 		}
 	}
- 	
-	private SalesTaxesProductDefinition productDescriptionParser(String productDescription)
-	{
+
+	private void calculateSalesTaxes(SalesTaxesProductDefinition salesTaxesProductDefinition) {
+		Optional<Product> product = iProductRepository
+				.findByNameIgnoreCase(salesTaxesProductDefinition.getProductName());
+		if (!product.isPresent()) {
+			// TODO: Change this to a erro msg propertie
+			throw new CustomException("Product type not found");
+		} else {
+			double tax = salesTaxesProductDefinition.getPrice() * (product.get().getProductType().getTax() / 100);
+
+			if (salesTaxesProductDefinition.isImported()) {
+				tax += salesTaxesProductDefinition.getPrice() * (Constants.IMPORTED_PRODUCT_TAX / 100);
+			}
+
+			tax = Utils.ceil(tax);
+			salesTaxesProductDefinition.setPriceWithTax(Utils.round(salesTaxesProductDefinition.getPrice() + tax));
+		}
+	}
+
+	private SalesTaxesProductDefinition productDescriptionParser(String productDescription) {
 		SalesTaxesProductDefinition salesTaxesProductDefinition = new SalesTaxesProductDefinition();
 		int firstBlanckSpace = productDescription.indexOf(Constants.BLANCK_SPACE);
-		try
-		{
-			String productQtyStr = productDescription.substring(0, firstBlanckSpace);
+		String productQtyStr = new String();
+		try {
+			productQtyStr = productDescription.substring(0, firstBlanckSpace);
 			int productQty = Integer.valueOf(productQtyStr);
 			salesTaxesProductDefinition.setQty(productQty);
-		}catch (NumberFormatException e) {
-			throw new IllegalArgumentException();
+		} catch (NumberFormatException e) {
+			// TODO: Change this to a error msg propertie
+			throw new CustomException("Product quantity '"+productQtyStr+"' is not an integer");
 		}
-		
+
 		boolean isImported = productDescription.contains(Constants.IMPORTED_PRODUCT_DESCRIPTION);
 		salesTaxesProductDefinition.setImported(isImported);
-		
+
 		String productName = null;
-		if(isImported)
-		{
-			productName = productDescription.substring( Constants.IMPORTED_PRODUCT_DESCRIPTION.length() + productDescription.indexOf(Constants.IMPORTED_PRODUCT_DESCRIPTION) + 1, 
-														productDescription.indexOf(" at"));	
-		}else
-		{
+		if (isImported) {
+			productName = productDescription.substring(
+					Constants.IMPORTED_PRODUCT_DESCRIPTION.length()
+							+ productDescription.indexOf(Constants.IMPORTED_PRODUCT_DESCRIPTION) + 1,
+					productDescription.indexOf(" at"));
+		} else {
 			productName = productDescription.substring(firstBlanckSpace + 1, productDescription.indexOf(" at"));
 		}
-		
+
 		salesTaxesProductDefinition.setProductName(productName);
-		
-		try
-		{
-			String productPriceStr = productDescription.substring(productDescription.lastIndexOf(Constants.BLANCK_SPACE)+1);
+
+		String productPriceStr = new String();
+		try {
+			productPriceStr = productDescription
+					.substring(productDescription.lastIndexOf(Constants.BLANCK_SPACE) + 1);
 			double productPrice = Double.valueOf(productPriceStr);
 			salesTaxesProductDefinition.setPrice(productPrice);
-		}catch (NumberFormatException e) {
-			throw new IllegalArgumentException();
+		} catch (NumberFormatException e) {
+			// TODO: Change this to a error msg propertie
+			throw new CustomException("Product price '"+productQtyStr+"' is not a floating point number");
 		}
-		
+
 		return salesTaxesProductDefinition;
 	}
-	
-	
+
 }
