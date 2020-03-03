ba734010a390324280b0a1b830d5523cff4ebf9f
Rafael Batista Duarte <rafaelbatistaduarte@gmail.com>
2020-03-02 21:41:14 -0300

    Fix merge

--- backend/implementation/src/main/java/com/br/liferay/sales/taxes/calculator/controller/impl/SalesTaxesController.java
+++ backend/implementation/src/main/java/com/br/liferay/sales/taxes/calculator/controller/impl/SalesTaxesController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.liferay.sales.taxes.calculator.dto.ProductsDescriptionDTO;
-import com.br.liferay.sales.taxes.calculator.dto.SalesTaxesDTO;
 import com.br.liferay.sales.taxes.calculator.model.ProductsDescriptions;
 import com.br.liferay.sales.taxes.calculator.service.impl.SalesTaxesService;
 import com.br.liferay.sales.taxes.calculator.utils.Constants;
